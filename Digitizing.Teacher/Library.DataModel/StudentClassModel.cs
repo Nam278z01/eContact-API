6d716d5d89446c5b7408d80779f284e4ceb13169
Hoang Quynh <hoangquynh12a3dqh@gmail.com>
2022-08-06 19:49:34 +0700

    Change class student detail

--- Digitizing.Teacher/Library.DataModel/StudentClassModel.cs
+++ Digitizing.Teacher/Library.DataModel/StudentClassModel.cs
@@ -20,6 +20,25 @@ namespace Library.DataModel
         public DateTime date_of_birth { get; set; }
         public string student_role { get; set; }
         public string student_status { get; set; }
+        public string student_nationality { get; set; }
+        public string student_religion { get; set; }
+        public string student_countryside { get; set; }
+        public string student_citizen_identity_card { get; set; }
+        public string student_facebook_url { get; set; }
+        public string father_name { get; set; }
+        public string father_year_of_birth { get; set; }
+        public string father_nationality { get; set; }
+        public string father_nation { get; set; }
+        public string father_permanent_residence { get; set; }
+        public string father_work { get; set; }
+        public string father_phone_number { get; set; }
+        public string mother_name { get; set; }
+        public string mother_year_of_birth { get; set; }
+        public string mother_nationality { get; set; }
+        public string mother_nation { get; set; }
+        public string mother_permanent_residence { get; set; }
+        public string mother_work { get; set; }
+        public string mother_phone_number { get; set; }
 
     }
 }
